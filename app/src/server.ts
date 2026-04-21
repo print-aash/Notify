f59618cb0ed6729bb2b91cbc187100135efaf6ee
print-aash <ashusingh88106283@gmail.com>
2026-04-21 18:28:45 +0530

    db work

--- app/src/server.ts
+++ app/src/server.ts
@@ -3,11 +3,22 @@
  */
 import express from "express";
 import env from "dotenv";
+import mongoose from "mongoose";
 
 env.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 8080;
 
+//Mongoose
+export const ConecntDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI!);
+    console.log("Db Conected");
+  } catch (err) {
+    console.error(err);
+  }
+};
+ConecntDb();
 app.get("/", async (req, res) => {
   res.send("Hi");
 });
